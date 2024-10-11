83fa7a0b8c62d0f04612c32ec4900aa47f03573a
Oskar Lothe <osklot12@gmail.com>
2024-10-11 14:55:33 +0200

    Added heatmap function

--- diabetes-class-assignment/diabetes-class-assignment/main.py
+++ diabetes-class-assignment/diabetes-class-assignment/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+import seaborn as sns
 from matplotlib import pyplot as plt
 import os
 
@@ -13,6 +13,7 @@ def main():
         "BMI","GenHlth","Age","Education","Income"
     ])
     print(corr_matrix(data)["Diabetes_binary"].sort_values(ascending=False))
+    heatmap(corr_matrix(data))
 
 def get_absolute_path(relative_path):
     base_path = os.path.dirname(__file__)
@@ -26,6 +27,13 @@ def histogram(data, attributes):
     data[attributes].hist(figsize=(16, 12), bins=30, edgecolor='black')
     plt.tight_layout()
     plt.show()
+
+
+def heatmap(data):
+    plt.figure(figsize=(20, 20))
+    sns.heatmap(data, annot=True, cmap="coolwarm", vmin=-1, vmax=1)
+    plt.show()
+
 
 def corr_matrix(data):
     return data.corr()
