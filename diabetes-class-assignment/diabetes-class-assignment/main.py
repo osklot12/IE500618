082bb85662185c2acc1c9ca3b49f7856695912d7
Oskar Lothe <osklot12@gmail.com>
2024-10-12 10:09:11 +0200

    Done some preprocessing

--- diabetes-class-assignment/diabetes-class-assignment/main.py
+++ diabetes-class-assignment/diabetes-class-assignment/main.py
@@ -1,7 +1,14 @@
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 import os
+
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import FunctionTransformer, StandardScaler
+
 
 def main():
     data = load_dataframe("../../datasets/diabetes/diabetes_binary_classification_data.csv")
@@ -13,15 +20,38 @@ def main():
         "BMI","GenHlth","Age","Education","Income"
     ])
     print(corr_matrix(data)["Diabetes_binary"].sort_values(ascending=False))
-    heatmap(corr_matrix(data))
+
+    # splitting dataset
+    train_set, test_set = train_test_split(data, 0.2, random_state=42)
+    diabetes = train_set.drop(columns=["Diabetes_binary"], axis=1)
+    diabetes = train_set["Diabetes_binary"]
+
+    # selecting features
+    log_features = ["BMI"]
+
+    # creating pipelines
+    log_pipeline = make_pipeline(
+        FunctionTransformer(np.log, feature_names_out="one_to_one"),
+        StandardScaler()
+    )
+
+    # creating complete preprocessing pipeline
+    preprocessing = ColumnTransformer([
+        ("log", log_pipeline, log_features),
+    ]
+    remainder=)
+
+
 
 def get_absolute_path(relative_path):
     base_path = os.path.dirname(__file__)
     absolute_path = os.path.join(base_path, relative_path)
     return absolute_path
 
+
 def load_dataframe(path):
     return pd.read_csv(get_absolute_path(path))
+
 
 def histogram(data, attributes):
     data[attributes].hist(figsize=(16, 12), bins=30, edgecolor='black')
